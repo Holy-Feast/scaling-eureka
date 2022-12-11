38ea1d7728ae91b49dff9fb3dd26f47e0536e6b5
Holy-Feast <shelofasstov@gmail.com>
2022-12-11 14:18:32 +0200

    init

---
+++ TelegramBot.php
@@ -0,0 +1,8 @@
+class TelegramBot
+{
+    protected $token = "5884323219:AAETkr0mnEFM7HRTrLJ6uJhtvgGfyP3AQcs";
+    protected function query($method, $params=[])
+    {
+        $url = ""
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+include('vendor/autoload.php');
+include('TelegramBot.php');
+echo "hello world";
+?>
